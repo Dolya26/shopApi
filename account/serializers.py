25ed53f5a7b87b9896c8026d68efdbdfb0db1b34
Dolya26 <doolat.daiyrkulov@gmail.com>
2023-01-18 21:12:25 +0600

    added login and category

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -27,5 +29,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
 
+    default_error_messages = {'bad_token': _('Token is invalid or expired!')}
 
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail('bad_token')
