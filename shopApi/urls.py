83299118b0f1ddddb3f1d428fcdf110b3ddf74cc
Dolya26 <doolat.daiyrkulov@gmail.com>
2023-01-19 20:32:04 +0600

    3 commit

--- shopApi/urls.py
+++ shopApi/urls.py
@@ -7,11 +7,12 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.routers import SimpleRouter
-
 from category.views import CategoryViewSet
+from product.views import ProductViewSet
 
 router = SimpleRouter()
 router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -34,3 +35,5 @@ urlpatterns = [
     path('api/v1/accounts/', include('account.urls')),
     path('api/v1/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
