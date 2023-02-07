9d9cacb2fe213f743366a7c6a54ffb70694acd3c
Dolya26 <doolat.daiyrkulov@gmail.com>
2023-02-07 18:53:51 +0600

    finally

--- account/send_mail.py
+++ account/send_mail.py
@@ -3,9 +3,10 @@ from django.core.mail import send_mail
 
 def send_confirmation_email(user, code):
     full_link = f"http://localhost:8000/api/v1/accounts/activate/{code}/"
+    full_link_server = f"http://35.246.223.100/api/v1/accounts/activate/{code}/"
     send_mail(
         'Здравствуйте, активируйте ваш аккаунт!',
-        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link}',
+        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link}\n{full_link_server}',
         'doolat.daiyrkulov@gmail.com',
         [user],
         fail_silently=False
@@ -20,8 +21,8 @@ def send_reset_email(user):
 
 def send_notification(user_email, order_id, price):
     send_mail(
-        'uvedomlenie o sozdanii zakaza!',
-        f"""vi sozdaly zakaz №{order_id}, \n polnaya stoimost' vashego zakaza: {price}. \nsposibo za to chto vibraly nas!""",
+        'Уведомление о создании заказа!',
+        f"""Вы создали заказ №{order_id}, \n полная стоимость вашего zakaza: {price}. \nsposibo za to chto vibraly nas!""",
         'from@example.com',
         [user_email],
         fail_silently=False
